ef2bcf28c676e7616e858249f713bdd5ed769751
Maurizio Pillitu <maoo@finos.org>
2020-03-27 05:34:14 +0100

    fixed link

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -105,7 +105,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('what-is-odp')}>
+              to={useBaseUrl('docs/what-is-odp')}>
               Get Started
             </Link>
           </div>
