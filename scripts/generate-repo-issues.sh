b6ea8f6eca6b99cdf78f8f93ed5418efe5259fda
Maurizio Pillitu <maoo@finos.org>
2020-04-01 01:28:35 +0200

    Tested issue generation (on contrib-toolbox) Works as expected. massive issue generation is still commented out

--- scripts/generate-repo-issues.sh
+++ scripts/generate-repo-issues.sh
@@ -2,35 +2,26 @@
 
 # This script generates the repo issues
 
-generate_post_data()
-{
-  cat <<EOF
-{
-  "title": "$ISSUE_TITLE",
-  "body": "$ISSUE_DESCRIPTION",
-  "labels": ["quality checks"]
-}
-EOF
-}
-
 if [ -z $ISSUE_TITLE ]; then
-  ISSUE_TITLE="[FINOS_PROJECT_BLUEPRINT] - Fixes to apply"
+  ISSUE_TITLE="[FINOS_PROJECT_BLUEPRINT] - Fixes to apply - test"
 fi
-
-rm -rf /tmp/existing-issues.txt
+export ISSUE_TITLE=$ISSUE_TITLE
 
 # Generate list of repos that already have an issue created with $ISSUE_TITLE
-curl -s -G "https://api.github.com/search/issues" --data-urlencode "q='$ISSUE_TITLE' in:title org:finos"  -H "Accept: application/vnd.github.preview" | jq '.items[] | (.repository_url/"/" | (.[-2] + "/" + .[-1]))' | tr -d '"' | sort | uniq > /tmp/existing-issues.txt
+rm -rf /tmp/existing-issues.txt
+curl -s -G "https://api.github.com/search/issues" --data-urlencode "q='$ISSUE_TITLE' in:title org:finos state:open"  -H "Accept: application/vnd.github.preview" | jq '.items[] | (.repository_url/"/" | (.[-2] + "/" + .[-1]))' | tr -d '"' | sort | uniq > /tmp/existing-issues.txt
 
+# Each validation item is parsed and, unless there's already an open issue with the same title, it will create a new one, using the "message" field as issue text.
 cat metadata-tool/finos-repo-validation.json | jq -r '.[]| [.org, .["repo-name"]] | @tsv' |
   while IFS=$'\t' read -r ORG REPO; do
+    export ORG=$ORG
+    export REPO=$REPO
     ISSUE_EXIST=`cat /tmp/existing-issues.txt | grep "$ORG/$REPO"`
     if [ -z "$ISSUE_EXIST" ]; then
-        cat finos-repo-validation.json | jq -r '.[] | select(.org == env.ORG and .["repo-name"] == env.REPO) | .message' > /tmp/$ORG-$REPO-issue-description.txt
+        PAYLOAD=`cat finos-repo-validation.json | jq -r '.[] | select(.org == env.ORG and .["repo-name"] == env.REPO) | { "title": env.ISSUE_TITLE, "body": .message, "labels":["quality checks"] }'`
+        ISSUE_DESCRIPTION=`cat /tmp/$ORG-$REPO-issue-description.txt`
         echo "Creating issue for repo $ORG/$REPO; text generated into /tmp/$ORG-$REPO-issue-description.txt"
         echo "Still disabled! Waiting for community announcement"
-        # curl -d "$(generate_post_data)" https://api.github.com/repos/$ORG/$REPO/issues
-        # echo curl -d "$(generate_post_data)" https://api.github.com/repos/$ORG/$REPO/issues
-        echo --------
+        # curl -v -u admin:$GITHUB_TOKEN -d "$PAYLOAD" https://api.github.com/repos/$ORG/$REPO/issues
     fi
   done
