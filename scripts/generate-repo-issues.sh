deaba91d658fe319be8fe8bf87780f71dc65920a
Maurizio Pillitu <maoo@finos.org>
2020-04-02 20:01:45 +0200

    pull existing issues across all finos orgs

--- scripts/generate-repo-issues.sh
+++ scripts/generate-repo-issues.sh
@@ -17,16 +17,18 @@ export ISSUE_TITLE=$ISSUE_TITLE
 # TODO - remove this when the feature is completely rolled out
 enabled_repos="
   contrib-toolbox,open-developer-platform,metadata-tool,bot-github-chatops,SymphonyElectron,electron-builder,electron-utils,electron-spellchecker,node-ffi,ScreenSnippet,ScreenSnippet2,SwiftSearch,electron-dl,ScreenShareIndicatorFrame,node-auto-launch"
-
-# TODO - add Symphony Eletron repos
-
+orgs="finos,symphonyoss,HadoukenIO,finos-plexus"
 
 # Generate list of repos that already have an issue created with $ISSUE_TITLE
-rm -rf /tmp/existing-issues.txt
-curl -s -G "https://api.github.com/search/issues" --data-urlencode "q='$ISSUE_TITLE' in:title org:finos state:open"  -H "Accept: application/vnd.github.preview" | jq '.items[] | (.repository_url/"/" | (.[-2] + "/" + .[-1]))' | tr -d '"' | sort | uniq > /tmp/existing-issues.txt
+rm -rf /tmp/existing-issues.txt ; touch /tmp/existing-issues.txt
+for ORG in $(echo $orgs | sed "s/,/ /g")
+do
+  echo "Pulling issues from github.com/$ORG"
+  curl -s -G "https://api.github.com/search/issues" --data-urlencode "q='$ISSUE_TITLE' in:title org:$ORG state:open"  -H "Accept: application/vnd.github.preview" | jq '.items[] | (.repository_url/"/" | (.[-2] + "/" + .[-1]))' | tr -d '"' | sort | uniq >> /tmp/existing-issues.txt
+done
 
 # Each validation item is parsed and, unless there's already an open issue with the same title, it will create a new one, using the "message" field as issue text.
-cat metadata-tool/finos-repo-validation.json | jq -r '.[]| [.org, .["repo-name"]] | @tsv' |
+cat finos-repo-validation.json | jq -r '.[]| [.org, .["repo-name"]] | @tsv' |
   while IFS=$'\t' read -r ORG REPO; do
     if [ -n "$ORG" ] && [ -n "$REPO" ]; then
       if echo "$enabled_repos" | grep -q ",$REPO,"
