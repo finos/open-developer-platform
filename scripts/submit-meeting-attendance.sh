f6328cc8702f4990290979d13a1a62a893c28de1
Maurizio Pillitu <maoo@finos.org>
2020-05-07 12:46:27 +0200

    Update submit-meeting-attendance.sh

--- scripts/submit-meeting-attendance.sh
+++ scripts/submit-meeting-attendance.sh
@@ -3,9 +3,10 @@
 # This script sumits the meeting attendance for the passed list of assignees
 
 cd ${GIT_CSV_REPO}-checkout
-echo "new entries..."
+echo "==== new entries..."
 diff ../${GIT_CSV_REPO}-checkout/github-finos-meetings.csv github-finos-meetings.csv
 cp -f ../${GIT_CSV_REPO}-checkout/github-finos-meetings.csv github-finos-meetings.csv
+echo "end of new entries ===="
 
 if [ "$ACTION" == "closed" ]; then
     cat ../metadata-tool/github-finos-meetings-add.csv >> github-finos-meetings.csv
